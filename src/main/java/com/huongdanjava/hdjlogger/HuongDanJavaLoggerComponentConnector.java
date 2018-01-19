f938e7d3d9972a54a6772421791a6150e8471acd
huongdanjavacom <huongdanjava.com@gmail.com>
2018-01-19 08:45:10 +0700

    #4 Add setting UI

--- src/main/java/com/huongdanjava/hdjlogger/HuongDanJavaLoggerComponentConnector.java
+++ src/main/java/com/huongdanjava/hdjlogger/HuongDanJavaLoggerComponentConnector.java
@@ -1,10 +1,17 @@
 package com.huongdanjava.hdjlogger;
 
+import org.mule.api.MuleEvent;
 import org.mule.api.annotations.Config;
 import org.mule.api.annotations.Connector;
 import org.mule.api.annotations.Processor;
+import org.mule.api.annotations.display.FriendlyName;
+import org.mule.api.annotations.display.Placement;
+import org.mule.api.annotations.param.Default;
+import org.mule.api.annotations.param.Optional;
 
 import com.huongdanjava.hdjlogger.config.ConnectorConfig;
+import com.huongdanjava.hdjlogger.constant.LoggingLevel;
+import com.huongdanjava.hdjlogger.constant.Text;
 
 @Connector(name="huong-dan-java-logger-component", friendlyName="Huong Dan Java Logger")
 public class HuongDanJavaLoggerComponentConnector {
@@ -12,18 +19,16 @@ public class HuongDanJavaLoggerComponentConnector {
     @Config
     ConnectorConfig config;
 
-    /**
-     * Custom processor
-     *
-     * @param friend Name to be used to generate a greeting message.
-     * @return A greeting message
-     */
     @Processor
-    public String logging() {
-        /*
-         * MESSAGE PROCESSOR CODE GOES HERE
-         */
-        return config.getCorrelationId();
+    public Object log(
+		@Placement(group = Text.GENERIC, order = 1) @Optional String message,
+		@Placement(group = Text.GENERIC, order = 2) @Default("INFO") @FriendlyName(Text.LEVEL) LoggingLevel loggingLevel,
+
+		@Placement(group = Text.OVERRIDE, order = 1) @Optional @FriendlyName(Text.OVVERIDE_CORRELATION_ID) String globalCorrelationIdOverride,
+		@Placement(group = Text.OVERRIDE, order = 2) @Optional @FriendlyName(Text.OVVERIDE_CATEGORY) String overrideGlobalCategoryOverride,
+		MuleEvent event) {
+
+        return event.getMessage().getPayload();
     }
 
     public ConnectorConfig getConfig() {
--- src/main/java/com/huongdanjava/hdjlogger/constant/Text.java
+++ src/main/java/com/huongdanjava/hdjlogger/constant/Text.java
@@ -4,5 +4,10 @@ public class Text {
 
 	public static final String GENERIC = "Generic";
 	public static final String CORRELATION_ID = "Correlation Id";
+	public static final String LEVEL = "Level";
+	public static final String OVERRIDE = "Override Global Configuration";
+	public static final String OVVERIDE_CORRELATION_ID = "Corellation Id";
+	public static final String OVVERIDE_CATEGORY = "Category";
+
 
 }
