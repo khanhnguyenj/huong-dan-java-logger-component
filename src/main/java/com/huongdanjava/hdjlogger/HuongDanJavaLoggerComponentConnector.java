ba91ee787fb79502ef2e80f34b51a3607f794263
huongdanjavacom <huongdanjava.com@gmail.com>
2018-01-19 08:51:57 +0700

    #5 Move to Mule Component

--- src/main/java/com/huongdanjava/hdjlogger/HuongDanJavaLoggerComponentConnector.java
+++ src/main/java/com/huongdanjava/hdjlogger/HuongDanJavaLoggerComponentConnector.java
@@ -1,6 +1,7 @@
 package com.huongdanjava.hdjlogger;
 
 import org.mule.api.MuleEvent;
+import org.mule.api.annotations.Category;
 import org.mule.api.annotations.Config;
 import org.mule.api.annotations.Connector;
 import org.mule.api.annotations.Processor;
@@ -14,6 +15,7 @@ import com.huongdanjava.hdjlogger.constant.LoggingLevel;
 import com.huongdanjava.hdjlogger.constant.Text;
 
 @Connector(name="huong-dan-java-logger-component", friendlyName="Huong Dan Java Logger")
+@Category(name = "org.mule.tooling.category.core", description = "Components")
 public class HuongDanJavaLoggerComponentConnector {
 
     @Config
