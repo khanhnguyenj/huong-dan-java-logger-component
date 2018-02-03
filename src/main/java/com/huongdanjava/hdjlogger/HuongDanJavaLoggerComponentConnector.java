f681155412a8e412806cf0b6b2766fbd1fe3d3f7
huongdanjavacom <huongdanjava.com@gmail.com>
2018-02-03 11:41:00 +0700

    #8 Get message need to log

--- src/main/java/com/huongdanjava/hdjlogger/HuongDanJavaLoggerComponentConnector.java
+++ src/main/java/com/huongdanjava/hdjlogger/HuongDanJavaLoggerComponentConnector.java
@@ -2,6 +2,7 @@ package com.huongdanjava.hdjlogger;
 
 import org.apache.commons.lang.StringUtils;
 import org.mule.api.MuleEvent;
+import org.mule.api.MuleException;
 import org.mule.api.annotations.Category;
 import org.mule.api.annotations.Config;
 import org.mule.api.annotations.Connector;
@@ -10,6 +11,9 @@ import org.mule.api.annotations.display.FriendlyName;
 import org.mule.api.annotations.display.Placement;
 import org.mule.api.annotations.param.Default;
 import org.mule.api.annotations.param.Optional;
+import org.mule.api.expression.ExpressionManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
 import com.huongdanjava.hdjlogger.config.ConnectorConfig;
@@ -19,6 +23,8 @@ import com.huongdanjava.hdjlogger.constant.Text;
 @Connector(name="huong-dan-java-logger-component", friendlyName="Huong Dan Java Logger")
 @Category(name = "org.mule.tooling.category.core", description = "Components")
 public class HuongDanJavaLoggerComponentConnector {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(HuongDanJavaLoggerComponentConnector.class);
 
 	private static final String CORRELATION_ID_ELEMENT = "correlationId";
 
@@ -38,10 +44,25 @@ public class HuongDanJavaLoggerComponentConnector {
 
     	MDC.put(CORRELATION_ID_ELEMENT, correlationId);
 
+    	String evaluatedMessage = getEvaluatedMessage(event, message);
+
         return event.getMessage().getPayload();
     }
 
-    private String getCorrelationId(ConnectorConfig cc, String globalCorrelationIdOverride) {
+    private String getEvaluatedMessage(MuleEvent event, String message) {
+    	if (StringUtils.isEmpty(message)) {
+			try {
+				return event.getMessageAsString();
+			} catch (MuleException e) {
+				LOGGER.error(e.getMessage(), e);
+				return null;
+			}
+		}
+
+		return evaluateMEL(event, message);
+	}
+
+	private String getCorrelationId(ConnectorConfig cc, String globalCorrelationIdOverride) {
     	if (!StringUtils.isEmpty(globalCorrelationIdOverride)) {
 			return globalCorrelationIdOverride;
 		}
@@ -55,6 +76,12 @@ public class HuongDanJavaLoggerComponentConnector {
 		}
 
 		return cc.getCategory();
+	}
+
+    private String evaluateMEL(MuleEvent event, String text) {
+		ExpressionManager em = event.getMuleContext().getExpressionManager();
+
+		return em.parse(text, event);
 	}
 
     public ConnectorConfig getConfig() {
